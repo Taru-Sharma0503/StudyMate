c7a0eff3c5685e0fd4c4621185c6033db67acf9c
Taru Sharma <ps9667279@gmail.com>
2026-07-16 13:15:49 +0530

    Dns to ipv4

--- backend/src/config/nodemailer.js
+++ backend/src/config/nodemailer.js
@@ -1,14 +1,14 @@
+const dns = require("dns");
+dns.setDefaultResultOrder("ipv4first");
+
 const nodemailer = require("nodemailer");
 
 const transporter = nodemailer.createTransport({
-  host: "smtp.gmail.com",
-  port: 587,
-  secure: false, 
+  service: "gmail",
   auth: {
     user: process.env.EMAIL_USER,
     pass: process.env.EMAIL_APP_PASSWORD,
   },
-  family: 4, 
 });
 
 module.exports = transporter;
