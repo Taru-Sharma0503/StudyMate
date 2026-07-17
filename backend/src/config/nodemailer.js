4414382d51754d0bc86d0764f16bd04deab3904e
Taru Sharma <ps9667279@gmail.com>
2026-07-17 23:34:44 +0530

    Adding logger and debugger

--- backend/src/config/nodemailer.js
+++ backend/src/config/nodemailer.js
@@ -5,6 +5,8 @@ const transporter = nodemailer.createTransport({
   port: 587,
   secure: false,
   family: 4,
+  logger: true,
+  debug: true,
   auth: {
     user: process.env.EMAIL_USER,
     pass: process.env.EMAIL_APP_PASSWORD,
