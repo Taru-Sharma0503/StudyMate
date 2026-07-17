38ba3f87e84d33b1c8acb66156e31ccf7cceabf4
Taru Sharma <ps9667279@gmail.com>
2026-07-18 01:25:20 +0530

    Adding resend email services

--- backend/src/services/email.service.js
+++ backend/src/services/email.service.js
@@ -1,23 +1,23 @@
-const transporter = require("../config/nodemailer");
+const { Resend } = require("resend");
+
+const resend = new Resend(process.env.RESEND_API_KEY);
 
 async function sendEmail({ to, subject, text, html }) {
   if (!to || !subject) {
     throw new Error("Recipient (to) and subject are required to send an email");
   }
 
-  const mailOptions = {
-    from: process.env.EMAIL_USER,
+  const response = await resend.emails.send({
+    from: "onboarding@resend.dev",
     to,
     subject,
     text,
     html,
-  };
+  });
 
-  await transporter.verify();
-  console.log("SMTP verified");
-  const info = await transporter.sendMail(mailOptions);
-  console.log("Email sent:", info.messageId);
-  return info;
+  console.log("Email sent:", response.data?.id);
+
+  return response;
 }
 
 module.exports = sendEmail;
