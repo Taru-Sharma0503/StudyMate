e3a50858addd92af6924b68a73239fee2f0be006
Taru Sharma <ps9667279@gmail.com>
2026-07-17 23:32:47 +0530

    Forcing ipv4

--- backend/src/config/nodemailer.js
+++ backend/src/config/nodemailer.js
@@ -1,12 +1,10 @@
-const dns = require("dns");
-dns.setDefaultResultOrder("ipv4first");
-
 const nodemailer = require("nodemailer");
 
 const transporter = nodemailer.createTransport({
   host: "smtp.gmail.com",
   port: 587,
   secure: false,
+  family: 4,
   auth: {
     user: process.env.EMAIL_USER,
     pass: process.env.EMAIL_APP_PASSWORD,
