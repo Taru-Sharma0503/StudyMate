905f0302309c48fad2d00e683903716cbe6baa30
Taru Sharma <ps9667279@gmail.com>
2026-07-17 22:57:32 +0530

    Changing port

--- backend/src/config/nodemailer.js
+++ backend/src/config/nodemailer.js
@@ -4,7 +4,9 @@ dns.setDefaultResultOrder("ipv4first");
 const nodemailer = require("nodemailer");
 
 const transporter = nodemailer.createTransport({
-  service: "gmail",
+  host: "smtp.gmail.com",
+  port: 587,
+  secure: false,
   auth: {
     user: process.env.EMAIL_USER,
     pass: process.env.EMAIL_APP_PASSWORD,
